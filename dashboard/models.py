297fab9e874ac66f7fbb21fb2db5b0b44ed85e01
dedysutanto <dedysutanto@gmail.com>
2021-05-19 01:12:28 +0700

    Login Beautiful

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Dashboard(models.Model):
-    proxy = True
+#    proxy = True
 
     class Meta:
         verbose_name_plural = 'Dashboard'
