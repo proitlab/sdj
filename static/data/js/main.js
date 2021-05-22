fdc65907496013e14c127aba542992a627344a61
dedysutanto <dedysutanto@gmail.com>
2021-05-23 06:49:37 +0700

    Make verifikasi readonly if OK

--- static/data/js/main.js
+++ static/data/js/main.js
@@ -106,6 +106,7 @@ if($('#id_verifikasi').length){
     } else {
         $("#id_verifikasi_badge_ok").show();   
         $("#id_verifikasi_badge_nok").hide();
+        $("#id_verifikasi").prop("readonly", true);;
     }
 }else{
     $("#id_verifikasi_badge_ok").hide();   
